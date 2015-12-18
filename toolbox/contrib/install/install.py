842c298c58757c1828517dade9f4a91433e84b29
Jeffrey Slort <mail@jeffreyslort.nl>
2015-12-18 19:59:15 +0100

    install dev plugins with symlinks

--- toolbox/contrib/install/install.py
+++ toolbox/contrib/install/install.py
@@ -2,7 +2,7 @@ __author__ = 'jeff'
 
 from toolbox.plugin import ToolboxPlugin
 from toolbox.mixins import ConfigMixin
-import pip, os, shutil, inspect, importlib
+import pip, os, shutil, inspect, importlib, hashlib
 
 class InstallPlugin (ConfigMixin, ToolboxPlugin):
 
@@ -11,7 +11,7 @@ class InstallPlugin (ConfigMixin, ToolboxPlugin):
 
     def prepare_parser(self, parser):
         parser.add_argument("-u","--uninstall", action="store_true", help="Uninstall plugin")
-        parser.add_argument("-l", "--local", action='store_true', help="Install locally")
+        parser.add_argument("--dev", action='store_true', help="Install a dev plugin")
         parser.add_argument("-e", "--enable", action="store_true", help="enable existing package as toolbox plugin")
         parser.add_argument("-d", "--disable", action="store_true", help="disable an symlinked package")
         parser.add_argument("package")
@@ -26,12 +26,19 @@ class InstallPlugin (ConfigMixin, ToolboxPlugin):
         # check if given package is a local directory
         if os.path.exists(args.package):
             local_dir = self.get_config()['local_plugin_dir']
-            if command == 'install':
+            module_dir = os.path.abspath(args.package)
+            dir_hash = hashlib.md5(module_dir.encode('utf-8')).hexdigest()
+            symlink = os.path.join(self.get_config()['local_plugin_dir'], dir_hash)
+            if command == 'install' and args.dev:
+                self.link('enable', symlink, module_dir)
+            elif command == 'install':
                 try:
                     name = args.package.split('/')[-1]
                     shutil.copytree(args.package,os.path.join(local_dir,name))
                 except Exception as e:
                     print('Installation of {} failed with : {}'.format(args.package,e))
+            elif command == 'uninstall' and args.dev:
+                self.link('disable', symlink)
             elif command == 'uninstall':
                 try:
                     name = args.package.split('/')[-1]
@@ -53,9 +60,17 @@ class InstallPlugin (ConfigMixin, ToolboxPlugin):
 
             if args.enable:
                 # symlink the directory in the local plugin dir
-                os.symlink(module_dir,symlink , True)
+                self.link('enable', symlink, module_dir)
                 print("{} is enabled to use with Toolbox".format(args.package))
 
             if args.disable:
-                os.unlink(symlink)
+                self.link('disable', symlink)
                 print("{} is disabled".format(args.package))
+
+
+    def link(self, type,symlink, module_dir = None):
+        if type == 'enable':
+            os.symlink(module_dir,symlink , True)
+        elif type == 'disable':
+            os.unlink(symlink)
+
