dd89bd7195b0e7508dcf4ff7aca10fdd2624fc33
Jeffrey Slort <mail@jeffreyslort.nl>
2015-12-28 21:17:39 +0100

    fix wheel

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
     tests_require=test_requirements,
 
     packages=find_packages(),
+    include_package_data=True,
     entry_points = {
         'console_scripts': [
             'tbox = toolbox.cli:main',
