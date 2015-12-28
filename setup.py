bc994fee429548f34bb0773c9270ae7ba5c16c78
Jeffrey Slort <mail@jeffreyslort.nl>
2015-12-28 22:13:16 +0100

    Bump version: 0.3.6 → 0.3.7

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ test_requirements = [
 
 setup(
     name='tool-box',
-    version='0.3.6',
+    version='0.3.7',
     description="tools for your toolbox , neatly organised",
     long_description=readme + '\n\n' + history,
     author='Jeff Slort',
