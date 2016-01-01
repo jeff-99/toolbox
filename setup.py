408bbf2be58123de154b08c1cdda151a4d6986f0
Jeffrey Slort <mail@jeffreyslort.nl>
2016-01-01 23:07:23 +0100

    Bump version: 0.3.9 → 0.4.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ test_requirements = [
 
 setup(
     name='tool-box',
-    version='0.3.9',
+    version='0.4.0',
     description="tools for your toolbox , neatly organised",
     long_description=readme + '\n\n' + history,
     author='Jeff Slort',
--- toolbox/__init__.py
+++ toolbox/__init__.py
@@ -1,3 +1,3 @@
 __author__ = 'Jeffrey Slort'
 __email__ = 'j_slort@hotmail.com'
-__version__ = '0.3.9'
+__version__ = '0.4.0'
